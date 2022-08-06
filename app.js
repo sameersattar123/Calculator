a9c324daca961251b88a754a7ac8813f80d9ad95
sameer <sameersattar1111@gmail.com>
2022-08-06 12:13:34 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function getNumber(num)
+{
+    var result = document.getElementById("result")
+    result.value += num
+
+}
+
+function clearNumber()
+{
+    var result = document.getElementById("result");
+    result.value = ""
+}
+
+function getResult()
+{
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
